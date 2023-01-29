1011acb319435e395b1ba6e8593c28de1531473b
Lucas Rangit MAGASWERAN <lucasrangit@gmail.com>
2023-01-29 22:20:12 +0100

    simplify error handling in parser and limit section to first list of podcasts, the subscriptions, print summary

--- scrape.js
+++ scrape.js
@@ -3,36 +3,29 @@
  * @returns { name, url }
  */
 function getGooglePodcastUrlFromFile() {
+    // const links = document.querySelectorAll('scrolling-carousel span a');
     const scrollingCarousel = document.querySelector('scrolling-carousel span');
     const links = scrollingCarousel.getElementsByTagName('a');
-    var podcasts = {};
+    let podcasts = {};
     for (let i = 0; i < links.length; i++) {
         const link = links[i];
         try {
-            const img = link.getElementsByTagName('img')[0];
-            if (!img) {
-                continue;
-            }
+            const img = link.querySelector('img');
             const name = img.alt;
-            if (name === '') {
-                continue;
-            }
             const url = link.href;
-            if (url === '') {
-                continue;
-            }
             podcasts[name] = url;
         } catch (e) {
-            console.log(e);
             const divs = link.getElementsByTagName("div");
             let currentDiv = divs[0];
             for (let i = 0; i < 3; i++) {
                 currentDiv = currentDiv.getElementsByTagName("div")[0];
             }
-            maybe_name = currentDiv.innerText;
-            console.log("Failed to get URL for: ", maybe_name );
+            possible_name = currentDiv.innerText;
+            console.log("Failed to get URL for: ", possible_name );
         }
     }
     return podcasts;
 }
-console.log(getGooglePodcastUrlFromFile());
+podcasts = getGooglePodcastUrlFromFile();
+console.log("Found ", Object.keys(podcasts).length, " podcasts in Subscriptions")
+console.log(podcasts);
